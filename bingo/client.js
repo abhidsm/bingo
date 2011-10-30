e6a1ff5f0e531b2dfaa10ec5440d515f8200855c
Abhilash M A <abhidsm@gmail.com>
2011-10-30 14:20:33 +0530

    Added the basic files for bingo game

---
+++ bingo/client.js
@@ -0,0 +1,198 @@
+var USER = { name: "", id: "", joinId: "", joinName: "" };
+
+//used to keep the most recent messages visible
+function scrollDown () {
+  $('#log').scrollTop(100000000000000000);
+  $("#entry").focus();
+}
+
+function addMessage(user, text, color) {
+  var messageElement = $(document.createElement("table"));
+
+  messageElement.addClass("message");
+
+  var content = '<tr>'
+              + '  <td class="user" style="color:'+color+';">' + user + ':</td>'
+              + '  <td class="msg-text">' + text  + '</td>'
+              + '</tr>'
+              ;
+  messageElement.html(content);
+
+  //the log is the stream that we view
+  $("#log").append(messageElement);
+
+  //always view the most recent message when it is added
+  scrollDown();
+}
+
+function clearMessage(){
+  $("#log").html(null);
+  $('#flash').html(null);
+}
+
+function displayMessage(data){
+    addMessage(data[0].user, data[0].text, 'blue');
+}
+
+function longPoll() {
+  //make another request
+  $.ajax({
+    cache: false,
+    dataType: 'json',
+    type: "GET",
+    url: "/receive",
+    data: "userId="+USER.id,
+    error: function () {
+        checkPartnerExistsOrNot();
+    },
+    success: function (json) {
+      if(json[0].data == 'left'){
+        clearMessage();
+        showLoad();
+        connect();
+      }else {
+        displayMessage(json);
+        longPoll();
+      }
+    }
+  });
+}
+
+//Check whether partner exists
+function checkPartnerExistsOrNot(){
+  $.ajax({
+    cache: false,
+    dataType: 'json',
+    type: "GET",
+    url: "/checkPartner",
+    data: "joinId="+USER.joinId,
+    success: function (json) {
+      if(json[0].data == 'Yes'){
+          //don't flood the servers on error, wait 10 seconds before retrying
+          setTimeout(longPoll, 10*1000);
+      }else{
+          $(window).unload();
+          clearMessage();
+          showConnect();
+      }
+    }
+  });
+}
+
+
+//transition the page to the loading screen
+function showLoad () {
+  $("#connect").hide();
+  $("#loading").show();
+  $("#toolbar").hide();
+  $("#log").hide();
+}
+
+//Transition the page to the state that prompts the user for a nickname
+function showConnect () {
+  $("#connect").show();
+  $("#loading").hide();
+  $("#toolbar").hide();
+  $("#user").focus();
+  $("#log").hide();
+}
+
+//transition the page to the main chat view, putting the cursor in the textfield
+function showChat () {
+  $("#toolbar").show();
+  $("#entry").focus();
+
+  $("#connect").hide();
+  $("#loading").hide();
+  $("#log").show();
+  scrollDown();
+}
+
+//submit a new message to the server
+function send(msg) {
+  addMessage(USER.name, msg, 'red');
+  jQuery.get("/send", {timestamp: (new Date()).getTime(), user: USER.name, text: msg, to: USER.joinId}, function (data) { }, "json");
+}
+
+//join the chat
+function connect() {
+  $.ajax({
+    type: "GET",
+    url: "/connect",
+    data: "userId="+USER.id+"&userName="+USER.name,
+    dataType: 'json',
+    error: function () {
+      setTimeout(connect, 10*1000);
+    },
+    success: function (json) {
+      console.log('Return data from connect: '+json.userId);
+      if(json != 'wait') {
+        console.log('First UserID: '+json.userId);
+        USER.joinId = json.userId;
+        USER.joinName = json.userName;
+        $('#flash').html("<p>You have joined to play with '"+ json.userName+"'</p><p>Thanks for trying this application. We are under development. Keep trying!</p>");
+        $('#loading').hide();
+        //showChat();
+        //longPoll();
+      }
+    }
+  });
+}
+
+$(document).ready(function() {
+
+  //submit new messages when the user hits enter if the message isnt blank
+  $("#entry").keypress(function (e) {
+    if (e.keyCode != 13 /* Return */) return;
+    var msg = $("#entry").attr("value").replace("\n", "");
+    if(msg.length == 0) return;
+    send(msg);
+    $("#entry").attr("value", ""); // clear the entry field.
+  });
+
+  //Leave the chat
+  $('#leave').click(function(){
+    $(window).unload();
+    clearMessage();
+    showConnect();
+  });
+
+  //try joining the chat when the user clicks the connect button
+  $("#connectButton").click(function () {
+    var user = $("#user").attr("value");
+    if(user.length != 0){
+        //lock the UI while waiting for a response
+        showLoad();
+        
+        //dont bother the backend if we fail easy validations
+        if (user.length > 50) {
+            alert("Name too long. 50 character max.");
+            showConnect();
+            return false;
+        }
+
+        //more validations
+        if (/[^\w_\-^!]/.exec(user)) {
+            alert("Bad character in name. Can only have letters, numbers, and '_', '-', '^', '!'");
+            showConnect();
+            return false;
+        }
+    
+        //Assigning User details
+        var randomnumber=Math.floor(Math.random()*11111);
+        USER.name = user;
+        USER.id = randomnumber;
+        connect();
+    }
+    return false;
+  });
+
+  //if we can, notify the server that we're going away.
+  $(window).unload(function () {
+    jQuery.get("/leave", {userId: USER.id, joinId: USER.joinId}, function (data) { }, "json");
+  });
+
+
+  showConnect();                          
+});
+
---
+++ bingo/server.js
@@ -0,0 +1,214 @@
+var http = require("http"),
+    sys = require("sys"),
+    url = require("url"),
+    qs = require("querystring"),
+    fs = require('fs'),
+    path = require('path');
+
+// Handles the feed push and querying.
+var feed = new function () {
+  var messages = [],
+      callbacks = [], joinCallback = null;
+  var firstUser = null;
+  var firstUserName = null;
+
+
+  this.appendMessage = function (json) {
+    console.log('inside appendmessage ');
+    // Append the new item.
+    messages.push( json );
+
+    // Log it to the console
+    console.log(": " + JSON.parse(json).text + " pushed");
+
+    // As soon as something is pushed, call the query callback
+    for (var i = 0; i < callbacks.length; i++){
+        console.log('Inside appendmessage. Message To:'+JSON.parse(json).to+' Receiver: '+callbacks[i].receiverId);
+        if(callbacks[i].receiverId == JSON.parse(json).to){
+            callbacks[i].callback([JSON.parse(json)]);  
+            callbacks.splice(i,1);
+        }
+    }
+
+    // Make sur we don't flood the server
+    while (messages.length > ITEMS_BACKLOG)
+      messages.shift();
+  };
+
+  this.query = function (since, receiverId, callback) {
+    var matching = [];
+
+    for (var i = 0; i < messages.length; i++) {
+        var message = JSON.parse(messages[i]);
+        console.log('Inside query. Message To:'+message.to+' Receiver: '+receiverId);
+        if (message.timestamp > since && message.to == receiverId)
+            matching.push(message);
+    }
+
+    if (matching.length != 0) {
+      callback(matching);
+    } else {
+      callbacks.push({ timestamp: new Date(), callback: callback, receiverId: receiverId });
+    }
+  };
+
+  this.join = function(req, callback) {
+    var userId = qs.parse(url.parse(req.url).query).userId;
+    var userName = qs.parse(url.parse(req.url).query).userName;
+    if(firstUser == null || firstUser == userId){
+      firstUser = userId;
+      firstUserName = userName;
+      joinCallback = callback;
+    } else {
+      callback({userId:firstUser, userName: firstUserName}); 
+      joinCallback({userId:userId, userName: userName});
+      firstUser = null;
+      firstUserName = null;
+    }
+  };
+
+  this.checkPartner = function (joinId, callback) {
+    console.log('inside checkPartner');
+
+      // As soon as something is pushed, call the query callback
+      for (var i = 0; i < callbacks.length; i++){
+          console.log('Inside checkPartner. Check Partner:'+joinId+' User Id: '+callbacks[i].receiverId);
+          if(callbacks[i].receiverId == joinId){
+              callback(JSON.stringify({data: 'Yes'}));  
+              return;
+          }
+      }
+      callback(JSON.stringify({data: 'No'}));  
+  };
+
+};
+
+var ITEMS_BACKLOG = 20;
+
+var urlMap = {
+  '/connect' : function (req, res) {
+    console.log('Connect action');
+    feed.join(req, function (data) {
+        res.simpleJSON(200, data);
+    });
+  },
+  '/receive' : function (req, res) {
+    console.log('Receive action');
+    var since = parseInt(qs.parse(url.parse(req.url).query).timestamp, 10);
+    var receiverId = qs.parse(url.parse(req.url).query).userId;
+    feed.query(since, receiverId, function (data) {
+      res.simpleJSON(200, data);
+    });
+  },
+  '/send' : function (req, res) {
+    console.log('Send action ');
+    var parameters = qs.parse(url.parse(req.url).query);
+    var user = parameters.user;
+    console.log('User: '+user);
+    var text = parameters.text;
+    console.log('Text: '+text);
+    var timestamp = parameters.timestamp;
+    console.log('Timestamp: '+timestamp);
+    var to = parameters.to;
+    console.log('To: '+to);
+    feed.appendMessage(JSON.stringify({timestamp: timestamp, user: user, text: text, to: to}));
+    console.log('After calling appendmessage ');
+    res.simpleJSON(200, {});
+  },
+  '/leave' : function (req, res) {
+    console.log('leave action ');
+    var parameters = qs.parse(url.parse(req.url).query);
+    var userId = parameters.userId;
+    console.log('User: '+userId);
+    var joinId = parameters.joinId;
+    console.log('Join Id: '+joinId);
+    feed.appendMessage(JSON.stringify({data: "left", to: joinId}));
+    console.log('After calling leave');
+    res.simpleJSON(200, {});
+  },
+  '/checkPartner' : function (req, res) {
+    console.log('checkPartner action ');
+    var parameters = qs.parse(url.parse(req.url).query);
+    var joinId = parameters.joinId;
+    console.log('Join Id: '+joinId);
+    feed.checkPartner(joinId, function (data) {
+      res.simpleJSON(200, data);
+    });
+    console.log('After calling leave');
+  }
+
+};
+
+
+var NOT_FOUND = "Not Found\n";
+
+function notFound(req, res) {
+  res.writeHead(404, { "Content-Type": "text/plain"
+                     , "Content-Length": NOT_FOUND.length
+                     });
+  res.end(NOT_FOUND);
+}
+
+http.createServer(function (req, res) {
+
+  console.log('request starting...');
+    var filePath = req.url;
+    if (filePath == '/')
+      filePath = '/index.html';
+    filePath = __dirname+filePath;
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+     
+    console.log('Path: '+filePath);
+    path.exists(filePath, function(exists) {
+     
+        if (exists) {
+            fs.readFile(filePath, function(error, content) {
+                if (error) {
+                    res.writeHead(500);
+                    res.end();
+                }
+                else {
+                    console.log('File found: '+ filePath);
+                    res.writeHead(200, { 'Content-Type': contentType });
+                    res.end(content, 'utf-8');
+                }
+            });
+        }else {
+            console.log('File not exist: '+filePath);
+            // Get the url and associate the function to the handler
+            // or
+            // Trigger the 404
+            var action = url.parse(req.url).pathname;
+            var handler  = urlMap[action] || notFound;
+            console.log('url parsing: '+action);
+            console.log('handler: '+handler);
+            var json = "";
+
+                // We need to process the post but we need to wait until the request's body is available to get the field/value pairs.
+            req.body = '';
+            
+            res.simpleJSON = function (code, obj) {
+                var body = JSON.stringify(obj);
+                res.writeHead(code, {
+                                  "Content-Type": "text/json",
+                                  "Content-Length": body.length
+                              });
+                res.end(body);
+            };
+            handler(req, res);
+        }
+                    
+    });
+
+
+}).listen(12368);
+
